ceecceb1d3a5f561dcc663a431ba7d748f83dfd6
ameRese <yarukix@gmail.com>
2024-06-26 20:43:15 +0900

    refactor: Separated functions into files

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import { fetchHistoryData } from "./my-modules/fetch-history-data";
+import { submitData } from "./my-modules/submit-data";
 
 // 設定情報
 const firebaseConfig = {
@@ -18,44 +20,11 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Cloud Firestoreから取得したデータを表示する
-const fetchHistoryData = async() => {
-    let tags = "";
-
-    // reportsコレクションのデータを取得
-    const querySnapshot = await getDocs(collection(db, "reports"));
-
-    // データをテーブル表の形式に合わせてHTMLに挿入
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`;
-        document.getElementById("js-history").innerHTML = tags;
-    });
-};
-
-// Cloud Firestoreから取得したデータを表示する
 if (document.getElementById("js-history")) {
-    fetchHistoryData();
+    fetchHistoryData(getDocs, collection, db);
 }
 
 // Cloud Firestoreにデータを送信する
-const submitData = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-
-    try {
-        const docRef = await addDoc(collection(db, "reports"), {
-            date: new Date(),
-            name: formData.get("name"),
-            work: formData.get("work"),
-            comment: formData.get("comment")
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-};
-
-// Cloud Firestoreにデータを送信する
 if (document.getElementById("js-form")) {
-    document.getElementById("js-form").addEventListener("submit", (e) => submitData(e));
+    document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
 }

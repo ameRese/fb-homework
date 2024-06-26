87eb20b9293e346f1373b6d402e705b2f5188993
ameRese <yarukix@gmail.com>
2024-06-26 20:21:39 +0900

    feat: Updated the input page to work with Cloud Firebase

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// 設定情報
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to the service
+// Cloud Firestoreの初期化
 const db = getFirestore(app);
 
 // Cloud Firestoreから取得したデータを表示する
 const fetchHistoryData = async() => {
-    let tags = '';
+    let tags = "";
 
     // reportsコレクションのデータを取得
     const querySnapshot = await getDocs(collection(db, "reports"));
@@ -31,11 +28,34 @@ const fetchHistoryData = async() => {
     querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
         tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`;
-        document.getElementById('js-history').innerHTML = tags;
+        document.getElementById("js-history").innerHTML = tags;
     });
 };
 
 // Cloud Firestoreから取得したデータを表示する
-if (document.getElementById('js-history')) {
+if (document.getElementById("js-history")) {
     fetchHistoryData();
+}
+
+// Cloud Firestoreにデータを送信する
+const submitData = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    try {
+        const docRef = await addDoc(collection(db, "reports"), {
+            date: new Date(),
+            name: formData.get("name"),
+            work: formData.get("work"),
+            comment: formData.get("comment")
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+};
+
+// Cloud Firestoreにデータを送信する
+if (document.getElementById("js-form")) {
+    document.getElementById("js-form").addEventListener("submit", (e) => submitData(e));
 }
